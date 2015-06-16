c81049ed653e89e21470c1fcee6010dcf8ee9c3d
Simon <kaltsimon@gmail.com>
2015-06-16 11:50:15 +0200

    Added JPA data classes and hibernate configuration

---
+++ src/main/java/de/fu_berlin/agdb/crepe/data/StationMetaData.java
@@ -0,0 +1,130 @@
+package de.fu_berlin.agdb.crepe.data;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vividsolutions.jts.geom.Point;
+import de.fu_berlin.agdb.crepe.json.serialize.PointDeserializer;
+import de.fu_berlin.agdb.crepe.json.serialize.PointSerializer;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "dwd_station_meta_data", schema = "public")
+public class StationMetaData implements Serializable {
+    private long stationId;
+
+    private Point stationPosition;
+    private Date fromDate;
+    private Date untilDate;
+    private Integer stationHeight;
+    private String stationName;
+    private String federalState;
+
+    @Id
+    @NotNull
+    @Column(name = "station_id")
+    public long getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(long stationId) {
+        this.stationId = stationId;
+    }
+
+    @NotNull
+    @Column(name = "station_position")
+    @Type(type = "org.hibernate.spatial.GeometryType")
+    @JsonSerialize(using = PointSerializer.class)
+    public Point getStationPosition() {
+        return stationPosition;
+    }
+
+    @JsonDeserialize(using = PointDeserializer.class)
+    public void setStationPosition(Point stationPosition) {
+        this.stationPosition = stationPosition;
+    }
+
+    @Column(name = "from_date")
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    @Column(name = "until_date")
+    public Date getUntilDate() {
+        return untilDate;
+    }
+
+    public void setUntilDate(Date untilDate) {
+        this.untilDate = untilDate;
+    }
+
+    @Column(name = "station_height")
+    public Integer getStationHeight() {
+        return stationHeight;
+    }
+
+    public void setStationHeight(Integer stationHeight) {
+        this.stationHeight = stationHeight;
+    }
+
+    @Column(name = "station_name")
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    @Column(name = "federal_state")
+    public String getFederalState() {
+        return federalState;
+    }
+
+    public void setFederalState(String federalState) {
+        this.federalState = federalState;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StationMetaData that = (StationMetaData) o;
+
+        if (stationId != that.stationId) return false;
+        if (stationPosition != null ? !stationPosition.equals(that.stationPosition) : that.stationPosition != null)
+            return false;
+        if (fromDate != null ? !fromDate.equals(that.fromDate) : that.fromDate != null) return false;
+        if (untilDate != null ? !untilDate.equals(that.untilDate) : that.untilDate != null) return false;
+        if (stationHeight != null ? !stationHeight.equals(that.stationHeight) : that.stationHeight != null)
+            return false;
+        if (stationName != null ? !stationName.equals(that.stationName) : that.stationName != null) return false;
+        if (federalState != null ? !federalState.equals(that.federalState) : that.federalState != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (stationId ^ (stationId >>> 32));
+        result = 31 * result + (stationPosition != null ? stationPosition.hashCode() : 0);
+        result = 31 * result + (fromDate != null ? fromDate.hashCode() : 0);
+        result = 31 * result + (untilDate != null ? untilDate.hashCode() : 0);
+        result = 31 * result + (stationHeight != null ? stationHeight.hashCode() : 0);
+        result = 31 * result + (stationName != null ? stationName.hashCode() : 0);
+        result = 31 * result + (federalState != null ? federalState.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/de/fu_berlin/agdb/crepe/json/serialize/PointDeserializer.java
@@ -0,0 +1,41 @@
+package de.fu_berlin.agdb.crepe.json.serialize;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
+import de.fu_berlin.agdb.crepe.rest.util.SimplePoint;
+
+import java.io.IOException;
+
+/**
+ * Class to deserialize {@link com.vividsolutions.jts.geom.Point} objects from JSON.
+ * Adapted from <a href="http://stackoverflow.com/questions/27624940/map-a-postgis-geometry-point-field-with-hibernate-on-spring-boot">Stackoverflow</a>.
+ */
+public class PointDeserializer extends JsonDeserializer<Point> {
+
+    /**
+     * SRID 26910 is used here
+     * that means:
+     * PROJCS["NAD83 / UTM zone 10N",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-123],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],AUTHORITY["EPSG","26910"],AXIS["Easting",EAST],AXIS["Northing",NORTH]]
+     * TODO: externalize value?!
+     */
+    private static final int SRID = 26910;
+    private static final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), SRID);
+
+    @Override
+    public Point deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        try {
+            SimplePoint jsonPoint = jp.readValueAs(SimplePoint.class);
+            return geometryFactory.createPoint(
+                    new Coordinate(jsonPoint.getLatitude(), jsonPoint.getLongitude())
+            );
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
