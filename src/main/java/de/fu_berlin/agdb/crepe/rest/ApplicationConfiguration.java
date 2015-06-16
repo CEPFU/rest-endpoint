afac111b47120163fc3600f1863f0056ea19d963
Simon <kaltsimon@gmail.com>
2015-06-16 14:34:24 +0200

    Register deserializers and subtypes with object mapper.

--- src/main/java/de/fu_berlin/agdb/crepe/rest/ApplicationConfiguration.java
+++ src/main/java/de/fu_berlin/agdb/crepe/rest/ApplicationConfiguration.java
@@ -3,6 +3,13 @@ package de.fu_berlin.agdb.crepe.rest;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import de.fu_berlin.agdb.crepe.algebra.Profile;
+import de.fu_berlin.agdb.crepe.algebra.operators.numeric.Equal;
+import de.fu_berlin.agdb.crepe.data.Attribute;
+import de.fu_berlin.agdb.crepe.data.Event;
+import de.fu_berlin.agdb.crepe.json.serialize.AttributeDeserializer;
+import de.fu_berlin.agdb.crepe.json.serialize.EqualDeserializer;
+import de.fu_berlin.agdb.crepe.json.serialize.ProfileDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +26,11 @@ public class ApplicationConfiguration {
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
 
-//        //region Register custom deserializers
-//        builder.deserializerByType(Profile.class, new ProfileDeserializer());
-//        builder.deserializerByType(Equal.class, new EqualDeserializer());
-//        builder.deserializerByType(Attribute.class, new AttributeDeserializer());
-//        //endregion
+        //region Register custom deserializers
+        builder.deserializerByType(Profile.class, new ProfileDeserializer());
+        builder.deserializerByType(Equal.class, new EqualDeserializer());
+        builder.deserializerByType(Attribute.class, new AttributeDeserializer());
+        //endregion
 
         builder.modulesToInstall(MyModule.class);
 
@@ -44,9 +51,9 @@ public class ApplicationConfiguration {
         @Override
         public void setupModule(SetupContext context) {
             super.setupModule(context);
-//            context.registerSubtypes(Event.class);
-//            context.registerSubtypes(Equal.class);
-//            context.registerSubtypes(Attribute.class);
+            context.registerSubtypes(Event.class);
+            context.registerSubtypes(Equal.class);
+            context.registerSubtypes(Attribute.class);
 //            context.setMixInAnnotations(Operator.class, JSONOutputAdapter.TypeInfoMixIn.class);
 //            context.setMixInAnnotations(IEvent.class, JSONOutputAdapter.TypeInfoMixIn.class);
 //            context.setMixInAnnotations(IAttribute.class, JSONOutputAdapter.TypeInfoMixIn.class);
