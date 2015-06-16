cb0f2954a85b619ce351d7d44b3db011aaff89b0
Simon <kaltsimon@gmail.com>
2015-06-16 17:10:50 +0200

    Added mixins for type info (de-)serialization

--- src/main/java/de/fu_berlin/agdb/crepe/rest/ApplicationConfiguration.java
+++ src/main/java/de/fu_berlin/agdb/crepe/rest/ApplicationConfiguration.java
@@ -3,13 +3,17 @@ package de.fu_berlin.agdb.crepe.rest;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import de.fu_berlin.agdb.crepe.algebra.Operator;
 import de.fu_berlin.agdb.crepe.algebra.Profile;
 import de.fu_berlin.agdb.crepe.algebra.operators.numeric.Equal;
 import de.fu_berlin.agdb.crepe.data.Attribute;
 import de.fu_berlin.agdb.crepe.data.Event;
+import de.fu_berlin.agdb.crepe.data.IAttribute;
+import de.fu_berlin.agdb.crepe.data.IEvent;
 import de.fu_berlin.agdb.crepe.json.serialize.AttributeDeserializer;
 import de.fu_berlin.agdb.crepe.json.serialize.EqualDeserializer;
 import de.fu_berlin.agdb.crepe.json.serialize.ProfileDeserializer;
+import de.fu_berlin.agdb.crepe.outputadapters.JSONOutputAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -54,9 +58,9 @@ public class ApplicationConfiguration {
             context.registerSubtypes(Event.class);
             context.registerSubtypes(Equal.class);
             context.registerSubtypes(Attribute.class);
-//            context.setMixInAnnotations(Operator.class, JSONOutputAdapter.TypeInfoMixIn.class);
-//            context.setMixInAnnotations(IEvent.class, JSONOutputAdapter.TypeInfoMixIn.class);
-//            context.setMixInAnnotations(IAttribute.class, JSONOutputAdapter.TypeInfoMixIn.class);
+            context.setMixInAnnotations(Operator.class, JSONOutputAdapter.TypeInfoMixIn.class);
+            context.setMixInAnnotations(IEvent.class, JSONOutputAdapter.TypeInfoMixIn.class);
+            context.setMixInAnnotations(IAttribute.class, JSONOutputAdapter.TypeInfoMixIn.class);
         }
     }
 }
