12bc16a2e9298884b179dadeadc0b8f2711706b9
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-07-15 12:31:12 +0200

    Add REST controller for new database schema

---
+++ src/main/java/de/fu_berlin/agdb/crepe/rest/controller/LocationMetaDataController.java
@@ -0,0 +1,14 @@
+package de.fu_berlin.agdb.crepe.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A REST controller to serve data about the available locations
+ * and allow the user to add new ones.
+ */
+@RestController
+@RequestMapping("/locations")
+public class LocationMetaDataController {
+    
+}
