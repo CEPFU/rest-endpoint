0d60c78492c1d2044106ea3365a7b21b2bbe39a2
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-07-15 20:43:52 +0200

    Don't use unavailable serializers

--- src/main/java/de/fu_berlin/agdb/crepe/rest/ApplicationConfiguration.java
+++ src/main/java/de/fu_berlin/agdb/crepe/rest/ApplicationConfiguration.java
@@ -1,20 +1,7 @@
 package de.fu_berlin.agdb.crepe.rest;
 
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import de.fu_berlin.agdb.crepe.algebra.Operator;
-import de.fu_berlin.agdb.crepe.algebra.Profile;
-import de.fu_berlin.agdb.crepe.algebra.operators.numeric.Equal;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.fu_berlin.agdb.crepe.core.Configuration;
-import de.fu_berlin.agdb.crepe.data.Attribute;
-import de.fu_berlin.agdb.crepe.data.Event;
-import de.fu_berlin.agdb.crepe.data.IAttribute;
-import de.fu_berlin.agdb.crepe.data.IEvent;
-import de.fu_berlin.agdb.crepe.json.serialize.AttributeDeserializer;
-import de.fu_berlin.agdb.crepe.json.serialize.EqualDeserializer;
-import de.fu_berlin.agdb.crepe.json.serialize.ProfileDeserializer;
-import de.fu_berlin.agdb.crepe.outputadapters.JSONOutputAdapter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilder;
@@ -64,39 +51,11 @@ public class ApplicationConfiguration {
 
     @Bean
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-
-        //region Register custom deserializers
-        builder.deserializerByType(Profile.class, new ProfileDeserializer());
-        builder.deserializerByType(Equal.class, new EqualDeserializer());
-        builder.deserializerByType(Attribute.class, new AttributeDeserializer());
-        //endregion
-
-        builder.modulesToInstall(MyModule.class);
-
-        return builder;
+        return new Jackson2ObjectMapperBuilder();
     }
-
 
     @Bean
-    public Module defaultModule() {
-        return new MyModule();
-    }
-
-    public static class MyModule extends SimpleModule {
-        public MyModule() {
-            super("MyModule", new Version(1, 0, 0, null, null, null));
-        }
-
-        @Override
-        public void setupModule(SetupContext context) {
-            super.setupModule(context);
-            context.registerSubtypes(Event.class);
-            context.registerSubtypes(Equal.class);
-            context.registerSubtypes(Attribute.class);
-            context.setMixInAnnotations(Operator.class, JSONOutputAdapter.TypeInfoMixIn.class);
-            context.setMixInAnnotations(IEvent.class, JSONOutputAdapter.TypeInfoMixIn.class);
-            context.setMixInAnnotations(IAttribute.class, JSONOutputAdapter.TypeInfoMixIn.class);
-        }
+    public ObjectMapper objectMapper() {
+        return jacksonBuilder().build();
     }
 }
